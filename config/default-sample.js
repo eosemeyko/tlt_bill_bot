821e2eabb599b2666c05943edf627d61098d61d9
Egor Semeiko <raziel2283@mail.ru>
2017-09-30 19:22:47 +0300

    Rename config file to sample. Added ignore config file in production.

---
+++ config/default-sample.js
@@ -0,0 +1,17 @@
+// Install - Copy file from default.js
+module.exports = {
+    token: 'TELEGRAM_TOKEN_BOT_KEY',
+    tokenTTL: 1440, // in seconds
+    server:{
+        host:'MIKBILL_URL'
+    },
+    db:{
+        host: 'localhost',
+        user: 'USER',
+        password: 'PASSWORD',
+        database: 'DB_NAME'
+    },
+    users: {
+        /* CHAR_ID: STUFF_ID */
+    }
+};
